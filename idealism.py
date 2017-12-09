e31c25de4ce9f358b3da0a9ebf759e1e269c75b6
Jacob Allen Utley <jacob@halfinjest.com>
2017-12-08 23:49:28 -0600

    Syntax changes.

--- idealism.py
+++ idealism.py
@@ -1,8 +1,8 @@
 import math
 import sys
 
-def get_idealism(transitions, ideal):
-	return math.log(2 - abs(1 - transitions / ideal), 2)
+def get_idealism(ideal, real):
+	return math.log(2 - abs(1 - real / ideal), 2)
 
 def get_transitions(bitstring):
 	transitions = ""
@@ -36,7 +36,9 @@ def main():
 
 		ideal = (len(formatted_bitstring) - 1) / 2.0
 
-		options.append(get_idealism(sum(int(bit) for bit in get_transitions(formatted_bitstring)), ideal))
+		real = sum(int(bit) for bit in get_transitions(formatted_bitstring))
+
+		options.append(get_idealism(ideal, real))
 
 	sys.stdout.write(str(min(options)) + "\n")
 
